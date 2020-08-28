4beafe7a0d84ed18794dcf6743858f1b7a386cdb
johnhjernestam <70264512+johnhjernestam@users.noreply.github.com>
2020-08-28 09:37:22 +0200

    introduktionskod

---
+++ introkod_syntax/syntax.py
@@ -0,0 +1,12 @@
+print("Hej TE19C")
+
+# skapar en variabel och tilldelar det värdet "John"
+name = "John"
+
+print(f"Ditt namn är {name}.")
+
+#float typomvandlar strängen till ett flyttal för att kunna räkna med det
+side = float(input("Ange kvadratens sida: ")) 
+area = side**2
+
+print(f"Area av kvadraten är {area} a.e")
