9d71b34f172fe761454284bcdddff1e8fd416568
johnhjernestam <70264512+johnhjernestam@users.noreply.github.com>
2020-09-09 13:42:27 +0200

    uppgifterna

---
+++ introkod_syntax/Räkna med Python/celsius.py
@@ -0,0 +1,5 @@
+C = float(input("Ange tempraturen i celsius: "))
+
+K = C + 273.15
+
+print(f"{C}Celsius är {K:.0f}Kelvin")
--- introkod_syntax/Räkna med Python/kelvin.py
+++ introkod_syntax/Räkna med Python/kelvin.py
@@ -1,5 +1,5 @@
-K = float(input("Ange tempraturen i Kelvin "))
+K = float(input("Ange tempraturen i Kelvin: "))
 
-C = K - 272.15
+C = K - 273.15
 
-print(f"{K}Kelvin - {C}C")
+print(f"{K}Kelvin - {C:.0f} C")
