241b9fbac2a2718ca7022985fc184f6e3581ced2
johnhjernestam <70264512+johnhjernestam@users.noreply.github.com>
2021-03-19 11:27:35 +0100

    Delete Dictionary directory

--- Dictionary/codeaong.py
+++
@@ -1,9 +0,0 @@
-import random as rnd
-
-siffror = {}
-
-for i in range(1,10):
-    slumptal = rnd.randint(1,6)
-    siffror[f"{i}"] = slumptal
-    
-print(siffror)
--- Dictionary/dictionaryintro.py
+++
@@ -1,8 +0,0 @@
-person = dict(
-    namn = "John", 
-    ålder = "17",
-    yrke = "Stockpro",
-    arbetsplats = "NTI"
-)
-
-print(f"{person['namn']} är {person['yrke']} och jobbar på {person['arbetsplats']}")
